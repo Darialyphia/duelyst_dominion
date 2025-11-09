52cb72cb31ebcdff1232d22c4d476c519cb042bc
Darialyphia <lo.pennequin@gmail.com>
2025-11-09 03:50:12 +0100

    feat: add Primus Shieldmaster minion to core set

---
+++ packages/engine/src/card/sets/core/neutral/primus-shieldmaster.ts
@@ -0,0 +1,37 @@
+import { PointAOEShape } from '../../../../aoe/point.aoe-shape';
+import { MinionOnEnterModifier } from '../../../../modifier/modifiers/on-enter.modifier';
+import { ProvokeModifier } from '../../../../modifier/modifiers/provoke.modifier';
+import { TARGETING_TYPE } from '../../../../targeting/targeting-strategy';
+import type { MinionBlueprint } from '../../../card-blueprint';
+import { singleMinionTargetRules } from '../../../card-utils';
+import { CARD_KINDS, CARD_SETS, FACTIONS, RARITIES } from '../../../card.enums';
+
+export const primusShieldmaster: MinionBlueprint = {
+  id: 'primus-shieldmaster',
+  name: 'Primus Shieldmaster',
+  description: '@Provoke@.',
+  cardIconId: 'minions/neutral_primus-shieldmaster',
+  kind: CARD_KINDS.MINION,
+  collectable: true,
+  setId: CARD_SETS.CORE,
+  faction: FACTIONS.NEUTRAL,
+  rarity: RARITIES.BASIC,
+  tags: [],
+  manaCost: 4,
+  runeCost: {
+    red: 1,
+    yellow: 1
+  },
+  atk: 3,
+  cmd: 6,
+  maxHp: 3,
+  getTargets(game, card) {
+    return singleMinionTargetRules.getPreResponseTargets(game, card, { required: false });
+  },
+  getAoe: () => new PointAOEShape(TARGETING_TYPE.ALLY_MINION),
+  canPlay: () => true,
+  async onInit(game, card) {
+    await card.modifiers.add(new ProvokeModifier(game, card));
+  },
+  async onPlay() {}
+};
